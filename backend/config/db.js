38d646cffd05e2939fe9a0826456f9d5918121e7
Ankurzhi <ankubabu1972@gmail.com>
2026-04-26 20:48:37 +0530

    Add Aiven for database

--- backend/config/db.js
+++ backend/config/db.js
@@ -24,37 +24,57 @@
 // });
 
 // module.exports = db;
-const mysql2 = require('mysql2');
-require('dotenv').config();
+// const mysql2 = require('mysql2');
+// require('dotenv').config();
 
-const pool = mysql2.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // ✅ ADD THIS
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+// const pool = mysql2.createPool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT, // ✅ ADD THIS
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
 
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+
+//   ssl: {
+//     rejectUnauthorized: false, // ✅ VERY IMPORTANT
+//   },
+
+//   connectTimeout: 10000, // ✅ optional but helpful
+// });
+
+// const db = pool.promise();
+
+// // Test connection
+// pool.getConnection((err, connection) => {
+//   if (err) {
+//     console.error('❌ Database connection failed:', err.message);
+//   } else {
+//     console.log('✅ MySQL connected successfully');
+//     connection.release();
+//   }
+// });
+
+// module.exports = db;
+
+
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+
+const db = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-
-  ssl: {
-    rejectUnauthorized: false, // ✅ VERY IMPORTANT
-  },
-
-  connectTimeout: 10000, // ✅ optional but helpful
-});
-
-const db = pool.promise();
-
-// Test connection
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('❌ Database connection failed:', err.message);
-  } else {
-    console.log('✅ MySQL connected successfully');
-    connection.release();
-  }
+  queueLimit: 0
 });
 
 module.exports = db;
